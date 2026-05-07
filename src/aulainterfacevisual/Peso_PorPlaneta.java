d96b9d5310d74541073b5d356c4cb3903cd61887
Darce-Me <discarnivora@gmail.com>
2026-05-07 19:39:31 -0300

    Finalização Peso_PorPlaneta

--- src/aulainterfacevisual/Peso_PorPlaneta.java
+++ src/aulainterfacevisual/Peso_PorPlaneta.java
@@ -1,23 +1,104 @@
-
 package aulainterfacevisual;
 
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
 
 public class Peso_PorPlaneta extends javax.swing.JFrame {
-    
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Peso_PorPlaneta.class.getName());
 
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Peso_PorPlaneta.class.getName());
 
     public Peso_PorPlaneta() {
         initComponents();
     }
-    
-    public void calcular(){
-        
-    }
-    public void verificarPlaneta(){
-        
-    }
 
+    public void verificarPlaneta() {
+        //Verifica se o número é válido e realiza os calculos
+
+        //Formatação do resultado
+        DecimalFormat df1 = new DecimalFormat("00.00");
+
+        int planetas = cb_Planetas.getSelectedIndex();
+        double pesoD = Double.parseDouble(txf_Peso.getText());
+
+        if (pesoD < 0) {
+            JOptionPane.showMessageDialog(null, "Número inválido");
+        } else {
+
+            try {
+                double resultado;
+                switch (planetas) {
+                    case 0:
+                        //mercúrio
+
+                        resultado = (pesoD / 10) * 3.6;
+                        JOptionPane.showMessageDialog(null, "O seu peso em Mercúrio é: " + df1.format(resultado));
+
+                        break;
+                    case 1:
+                        //vênus
+
+                        resultado = (pesoD / 10) * 8.7;
+                        JOptionPane.showMessageDialog(null, "O seu peso em Vênus é: " + df1.format(resultado));
+
+                        break;
+                    case 2:
+                        //terra
+
+                        resultado = (pesoD / 10) * 9.8;
+                        JOptionPane.showMessageDialog(null, "O seu peso na Terra é: " + df1.format(resultado));
+
+                        break;
+                    case 3:
+                        //marte
+
+                        resultado = (pesoD / 10) * 3.7;
+                        JOptionPane.showMessageDialog(null, "O seu peso em Marte é: " + df1.format(resultado));
+
+                        break;
+                    case 4:
+                        //jupiter
+
+                        resultado = (pesoD / 10) * 24.79;
+                        JOptionPane.showMessageDialog(null, "O seu peso em Júpiter é: " + df1.format(resultado));
+
+                        break;
+                    case 5:
+                        //saturno
+
+                        resultado = (pesoD / 10) * 10.44;
+                        JOptionPane.showMessageDialog(null, "O seu peso em Saturno é: " + df1.format(resultado));
+
+                        break;
+                    case 6:
+                        //urano
+
+                        resultado = (pesoD / 10) * 8.69;
+                        JOptionPane.showMessageDialog(null, "O seu peso em Urano é: " + df1.format(resultado));
+
+                        break;
+                    case 7:
+                        //netuno
+
+                        resultado = (pesoD / 10) * 11.15;
+                        JOptionPane.showMessageDialog(null, "O seu peso em Netuno é: " + df1.format(resultado));
+
+                        break;
+                    case 8:
+                        //lua
+
+                        resultado = (pesoD / 10) * 1.6;
+                        JOptionPane.showMessageDialog(null, "O seu peso na Lua é: " + df1.format(resultado));
+
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Caixa vazia!");
+            }
+        }
+
+    }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -25,18 +106,18 @@ public class Peso_PorPlaneta extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jl_pesoPlaneta = new javax.swing.JLabel();
-        jtx_Peso = new javax.swing.JTextField();
+        txf_Peso = new javax.swing.JTextField();
         cb_Planetas = new javax.swing.JComboBox<>();
         jb_Calcular = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jl_pesoPlaneta.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jl_pesoPlaneta.setText("Peso no planeta Mercúrio(Kg):");
 
-        jtx_Peso.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jtx_Peso.addActionListener(this::jtx_PesoActionPerformed);
+        txf_Peso.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        txf_Peso.addActionListener(this::txf_PesoActionPerformed);
 
         cb_Planetas.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         cb_Planetas.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Mercurio", "Vênus", "Terra", "Marte", "Júpiter ", "Saturno", "Urano ", "Netuno", "Lua" }));
@@ -44,6 +125,7 @@ public class Peso_PorPlaneta extends javax.swing.JFrame {
 
         jb_Calcular.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jb_Calcular.setText("Calcular Peso");
+        jb_Calcular.addActionListener(this::jb_CalcularActionPerformed);
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel2.setText("Calculo de Peso por Planeta");
@@ -56,15 +138,15 @@ public class Peso_PorPlaneta extends javax.swing.JFrame {
                 .addGap(44, 44, 44)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jl_pesoPlaneta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jtx_Peso))
+                    .addComponent(txf_Peso))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
-                .addComponent(cb_Planetas, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(34, 34, 34))
+                .addComponent(cb_Planetas, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(25, 25, 25))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jb_Calcular, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
+                    .addComponent(jLabel2)
+                    .addComponent(jb_Calcular, javax.swing.GroupLayout.PREFERRED_SIZE, 245, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -77,7 +159,7 @@ public class Peso_PorPlaneta extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jl_pesoPlaneta)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jtx_Peso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(txf_Peso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(cb_Planetas))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
                 .addComponent(jb_Calcular)
@@ -102,17 +184,25 @@ public class Peso_PorPlaneta extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jtx_PesoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtx_PesoActionPerformed
-        
-    }//GEN-LAST:event_jtx_PesoActionPerformed
+    private void txf_PesoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txf_PesoActionPerformed
+
+    }//GEN-LAST:event_txf_PesoActionPerformed
 
     private void cb_PlanetasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cb_PlanetasActionPerformed
+        //Altera o jLabel de peso
+
         String planetas = cb_Planetas.getSelectedItem().toString();
-        jl_pesoPlaneta.setText("Peso no planeta "+planetas+"(Kg): ");
+        jl_pesoPlaneta.setText("Peso no planeta " + planetas + "(Kg): ");
     }//GEN-LAST:event_cb_PlanetasActionPerformed
 
+    private void jb_CalcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jb_CalcularActionPerformed
+        //botão que realiza a operação
+
+        verificarPlaneta();
+    }//GEN-LAST:event_jb_CalcularActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -142,6 +232,6 @@ public class Peso_PorPlaneta extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton jb_Calcular;
     private javax.swing.JLabel jl_pesoPlaneta;
-    private javax.swing.JTextField jtx_Peso;
+    private javax.swing.JTextField txf_Peso;
     // End of variables declaration//GEN-END:variables
 }
